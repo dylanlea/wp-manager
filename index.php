8dda2c5a3e1c8c32a54400532e752aafbdae8994
Dylan Lea <dylanlea@hotmail.co.uk>
2021-08-16 18:12:16 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WPM Manager
+ * Description: WPM Manager
+ * Author: Dylan Lea
+ * Author URI: https://www.dylanlea.co.uk
+ * Version: 1.0.0
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
